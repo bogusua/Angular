8e6e2b99faf1cf7a4fd4a62fed9d029a35024512
budanov <budanov@rozetka.ua>
2023-11-12 20:40:51 +0200

    finish lesson_2

--- lesson_2/src/app/app-routing.module.ts
+++ lesson_2/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageOneComponent} from "./page-one/page-one.component";
+import {PageTwoComponent} from "./page-two/page-two.component";
+import {PageThreeComponent} from "./page-three/page-three.component";
+import {SliderComponent} from "./slider/slider.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SliderComponent},
+  {path: 'page-one', component: PageOneComponent },
+  {path: 'page-two', component: PageTwoComponent },
+  {path: 'page-three', component: PageThreeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lesson_2/src/app/app.module.ts
+++ lesson_2/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageThreeComponent } from './page-three/page-three.component';
+import { SliderComponent } from './slider/slider.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageThreeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
