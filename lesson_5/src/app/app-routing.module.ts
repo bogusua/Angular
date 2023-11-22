74f9bf0f31b72dc42a1258cd670279a6317aab96
budanov <budanov@rozetka.ua>
2023-11-22 17:20:08 +0200

    finish lesson 5

--- lesson_5/src/app/app-routing.module.ts
+++ lesson_5/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CardsContainerComponent} from "./cards-container/cards-container.component";
+import {UnorderedListComponent} from "./unordered-list/unordered-list.component";
 
 const routes: Routes = [
-  {path: '', component: CardsContainerComponent}
+  {path: '', component: CardsContainerComponent},
+  {path: 'list', component: UnorderedListComponent}
 ];
 
 @NgModule({
--- lesson_5/src/app/app.module.ts
+++ lesson_5/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsContainerComponent } from './cards-container/cards-container.component';
 import { CardBlockComponent } from './card-block/card-block.component';
+import { UnorderedListComponent } from './unordered-list/unordered-list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { ListItemInputComponent } from './list-item-input/list-item-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsContainerComponent,
-    CardBlockComponent
+    CardBlockComponent,
+    UnorderedListComponent,
+    ListItemComponent,
+    ListItemInputComponent
   ],
   imports: [
     BrowserModule,
